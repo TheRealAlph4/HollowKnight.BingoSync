02e445aa81fbdda0fa79abbba3676c1911758e2f
Alph4 <alph4@gmx.at>
2024-07-21 03:38:30 +0200

    Provide outwards interface for other mods to add new goals and gamemodes

---
+++ BingoSync/Interfaces/Goals.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BingoSync
+{
+    public static class Goals
+    {
+        public static Dictionary<string, BingoGoal> ProcessGoalsFile(string filepath)
+        {
+            return BingoTracker.ProcessGoalsFile(filepath);
+        }
+
+        public static Dictionary<string, BingoGoal> ProcessGoalsStream(Stream goalstream)
+        {
+            return BingoTracker.ProcessGoalsStream(goalstream);
+        }
+
+        public static Dictionary<string, BingoGoal> GetVanillaGoals()
+        {
+            return GameModesManager.GetVanillaGoals();
+        }
+
+        public static void RegisterGoalsForCustom(string groupName, Dictionary<string, BingoGoal> goals)
+        {
+            GameModesManager.RegisterGoalsForCustom(groupName, goals);
+        }
+
+        public static void AddGameMode(GameMode gameMode)
+        {
+            GameModesManager.AddGameMode(gameMode);
+        }
+    }
+}
